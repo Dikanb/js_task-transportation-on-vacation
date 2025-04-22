d87b74aa7834c10f90e21121dd4737ea39cc769c
Dikanb <dikanb1502@gmail.com>
2025-04-22 11:09:11 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,10 +7,12 @@ function calculateRentalCost(days) {
   const cost = 40;
   const discount = 20;
   const bigDiscount = 50;
+  const longPeriod = 7;
+  const shortPeriod = 3;
 
-  if (days < 3) {
+  if (days < shortPeriod) {
     return days * cost;
-  } else if (days < 7) {
+  } else if (days < longPeriod) {
     return days * cost - discount;
   } else {
     return days * cost - bigDiscount;
